2480a20d9ec5e7044d3cf72dcec81fc2df592d57
clubjk <jk@jkcybersecurity.com>
2023-07-16 18:35:39 -0500

    final prod version

--- my-app/src/App.jsx
+++ my-app/src/App.jsx
@@ -39,6 +39,19 @@ const App = () => {
   }
 
   //TODO
+  if (tours.length === 0) {
+    return (
+      <main>
+        <div className="title">
+          <h2>no tours left</h2>
+          <button className="btn" onClick={fetchTours}>
+            refresh
+          </button>
+        </div>
+      </main>
+    );
+  }
+
   return (
     <main>
       <Tours tours={tours} removeTour={removeTour} />
