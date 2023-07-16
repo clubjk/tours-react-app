b883aa465ac5d6505e57135863ca1154ef6d25a8
clubjk <jk@jkcybersecurity.com>
2023-07-16 15:31:03 -0500

    halfway finished

---
+++ my-app/src/Tour.jsx
@@ -0,0 +1,16 @@
+import React from "react";
+
+const Tour = ({ id, image, info, name, price }) => {
+  return (
+    <article className="single-tour">
+      <img src={image} alt={name} className="img" />
+      <span className="tour-price">${price}</span>
+      <div className="tour-info">
+        <h5>{name}</h5>
+        <p>{info}</p>
+      </div>
+    </article>
+  );
+};
+
+export default Tour;
---
+++ my-app/src/Tours.jsx
@@ -0,0 +1,21 @@
+import React from "react";
+import Tour from "./Tour";
+
+const Tours = ({ tours }) => {
+  return (
+    <section>
+      <div className="title">
+        <h2>our tours</h2>
+        <div className="title-underline"></div>
+      </div>
+      <div className="tours">
+        {tours.map((tour) => {
+          //console.log(tour);
+          return <Tour key={tour.id} {...tour} />;
+        })}
+      </div>
+    </section>
+  );
+};
+
+export default Tours;
